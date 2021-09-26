7a97dcc59488d1ee9d633b9b4c5ed532e982d5d6
Hardik <hardikganatra20@gmail.com>
2021-09-26 14:02:52 +0530

    Functions Covered
    AS Ep- 15

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function outer() {
-  for (var i = 0; i < 5; i++) {
-    function closure(x) {
-      setTimeout(() => {
-        console.log(x);
-      }, x * 1000);
-    }
-    closure(i);
-  }
-}
-outer();
+// - ##### What is function Statement
+function a() {}
+// - ##### What is function Expression
+var b = function a() {};
+
+// Difference between function Statement && function Expression
+// - Hoisting (function Statement entire function block is hoisted whereas in function Expression only the variable is hoisted)
+
+// - ##### What is Anonymous function
+// - ##### What is named function Expression
+// - ##### DIfference between params and arguments
+// - ##### What are first class functons
+// - ##### Arrpw Functions
